2fb1fdc14821cbf2493c29bb71eaf1c3319cf875
niliaranet <niliara@proton.me>
2025-05-03 20:30:21 +0200

    set up swagger - user section

--- laravel/app/Http/Controllers/API/RegisterController.php
+++ laravel/app/Http/Controllers/API/RegisterController.php
@@ -9,6 +9,42 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\JsonResponse;
 
+/**
+ *     @OA\Post(
+ *         path="/api/login",
+ *         summary="Login",
+ *         tags={"User management"},
+ *         @OA\RequestBody(
+ *             @OA\MediaType(
+ *                 mediaType="application/json",
+ *                 @OA\Schema(
+ *                      type="object",
+ *                      @OA\Property(property="name", type="string"),
+ *                      @OA\Property(property="password", type="string"),
+ *                 )
+ *             )
+ *         ),
+ *         @OA\Response(response="200", description="Success"),
+ *         @OA\Response(response="404", description="User not found or invalid password"),
+ *     ),
+ *     @OA\Post(
+ *         path="/api/register",
+ *         summary="Register",
+ *         tags={"User management"},
+ *         @OA\RequestBody(
+ *             @OA\MediaType(
+ *                 mediaType="application/json",
+ *                 @OA\Schema(
+ *                      type="object",
+ *                      @OA\Property(property="name", type="string"),
+ *                      @OA\Property(property="password", type="string"),
+ *                 )
+ *             )
+ *         ),
+ *         @OA\Response(response="200", description="Success"),
+ *         @OA\Response(response="500", description="Name already in use / Error"),
+ *     ),
+ */
 class RegisterController extends BaseController
 {
     /**
@@ -52,7 +88,7 @@ class RegisterController extends BaseController
                 "token" => $user->createToken("Touken")->plainTextToken,
                 "name" => $user->name,
             ];
-            return $this->sendResponse($success, "User login successfully.");
+            return $this->sendResponse($success, "User logged in successfully.");
         } else {
             return $this->sendError("Unauthorized: username/password invalid", [
                 "error" => "Unauthorized",
